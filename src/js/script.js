c7197549b9001e63ee2e8dccb08e0c148f9f07e4
kuomolew <kuomolew@gmail.com>
2020-01-18 12:05:11 +0200

    media breakpoints

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,26 @@ $( () => {
         slidesToShow: 4,
         arrows: false,
         dots: false,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+              }
+            },
+            {
+              breakpoint: 800,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+              }
+            }
+          ]
         
     });
 
